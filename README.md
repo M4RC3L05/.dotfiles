c862b4a4d05d88cd59b8c618e0b74a25e7f021dd
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-05-21 02:46:26 +0100

    Improve setup.sh
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,12 +2,17 @@
 
 My dotfiles to be configured with GNU Stow
 
+## Dependencies
+
+The setup requires `xz` and `curl` to run to successfully.
+For WLS `socat` must be installed has well.
+
 ## How to use
 
 Clone repo to home dir /home/\<username>
 
-Run stow
+Run setup.sh
 
 ```bash
-stow files
+./setup.sh
 ```
--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 
+set -e
+
+# Install nix
+curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
+. ~/.nix-profile/etc/profile.d/nix.sh
+
 # Install packages
 echo "==> Installing packages"
 nix-env -iA \
   nixpkgs.stow \
   nixpkgs.bat \
   nixpkgs.exa \
-  nixpkgs.fish
+  nixpkgs.fish \
+  nixpkgs.git
 
 # Setup fish shell
 echo "==> Add fish shell to shells"
@@ -15,14 +22,18 @@ sudo sh -c "echo $(which fish) >> /etc/shells"
 echo "==> Setting fish as the default user shell"
 chsh -s $(which fish)
 
+# Stow
+echo "==> Stowing"
+stow --no-folding files
+
+echo "==> Install fisher"
+fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update"
+
 # Setup asdf
 echo "==> Install asdf"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
-echo "==> Setup asdf fish shell completions"
-mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
-# Stow
-echo "==> Stowing"
-stow files
+echo "==> Setup asdf fish shell completions"
+mkdir -p ~/.config/fish/completions && ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
 echo "==> All done, you can reload/restart"
