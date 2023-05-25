52443fc8c38d36f40f2319fdac6c86861fdf0bac
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-05-26 00:49:44 +0100

    Add prime-run alias
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -8,6 +8,7 @@ if status is-interactive
     alias cat "bat --style=plain"
     alias ls "exa --color=auto --header --git"
     alias code="codium --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto"
+    alias prime-run "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only"
 
     # asdf
     source ~/.asdf/asdf.fish
