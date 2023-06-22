e0bfc2e32134976c7ef9660705c4abc95aea8604
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-06-22 01:16:19 +0100

    Add appimage path and remove code alias
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -4,10 +4,11 @@ if status is-interactive
     set -g hydro_color_prompt magenta
     set -g hydro_color_duration yellow
 
+    fish_add_path ~/AppImages
+
     # aliases
     alias cat "bat --style=plain"
     alias ls "exa --color=auto --header --git"
-    alias code "codium --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto"
     alias prime-run "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only"
 
     # asdf
