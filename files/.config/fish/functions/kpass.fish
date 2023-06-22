d20bc459f2b69061567a07ffcd2c1cd27c400901
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-06-22 01:15:55 +0100

    Make disown inline on kpass function
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- files/.config/fish/functions/kpass.fish
+++ files/.config/fish/functions/kpass.fish
@@ -1,4 +1,3 @@
 function kpass --argument-names db --description "Launch keepass with given db open based on secret-tool password"
-    fish -c "secret-tool lookup keepass $db | keepassxc --pw-stdin ~/secrets/$db.kdbx >/dev/null 2>&1; secret-tool lock --collection=\"keepass\"" >/dev/null 2>&1 &
-    disown
+    fish -c "secret-tool lookup keepass $db | keepassxc --pw-stdin ~/secrets/$db.kdbx >/dev/null 2>&1; secret-tool lock --collection=\"keepass\"" >/dev/null 2>&1 &; disown
 end
