778bf2dc882063ef694f874bd5effebf47598a7c
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-08-04 18:47:41 +0100

    Make cat use bat or batcat
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -10,7 +10,7 @@ if status is-interactive
     set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
 
     # aliases
-    alias cat "bat --style=plain"
+    alias cat "if command -v bat >/dev/null; bat $argv; else; batcat $argv; end;"
     alias ls "exa --color=auto --header --git"
     alias prime-run "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only"
 
