707e39196b099c4ad6aa59af90f9921c55ea7c11
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-09-01 15:24:28 +0100

    Run code in nvidia
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- files/.config/fish/functions/code.fish
+++ files/.config/fish/functions/code.fish
@@ -1,3 +1,4 @@
 function code --description 'Launches codium with wayland flags'
-  codium --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto --disable-gpu $argv >/dev/null 2>&1 &; disown
+  __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only code-oss --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto $argv >/dev/null 2>&1 &
+  disown
 end
