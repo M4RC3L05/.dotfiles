21214cb3f8e2c44c19528eb0bf146a47d810ca04
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-09-01 15:24:18 +0100

    Remove kpass function
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- files/.config/fish/functions/kpass.fish
+++
@@ -1,3 +0,0 @@
-function kpass --argument-names db --description "Launch keepass with given db open based on secret-tool password"
-    fish -c "secret-tool lookup keepass $db | keepassxc --pw-stdin ~/secrets/$db.kdbx >/dev/null 2>&1; secret-tool lock --collection=\"keepass\"" >/dev/null 2>&1 &; disown
-end
