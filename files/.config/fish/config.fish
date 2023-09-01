e5f7808a6c17db00285eba54699be345061975fe
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-09-01 15:24:50 +0100

    General updates and add brew
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -5,15 +5,19 @@ if status is-interactive
     set -g hydro_color_duration yellow
 
     fish_add_path ~/AppImages
+    fish_add_path ~/.local/bin
 
     # Docker
-    set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
+    # set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
 
     # aliases
-    alias cat "if command -v bat >/dev/null; bat $argv; else; batcat $argv; end;"
-    alias ls "exa --color=auto --header --git"
+    alias cat "bat --plain"
+    alias ls "exa --color=auto --header --git --icons"
     alias prime-run "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only"
 
     # asdf
-    source ~/.asdf/asdf.fish
+    source /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.fish
+
+    # brew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv) 
 end
