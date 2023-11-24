430a22bba44d4369271029282a1a4ede8dfe12b0
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-11-24 13:19:53 +0000

    Do not force nvidia starting code
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- files/.config/fish/functions/code.fish
+++ files/.config/fish/functions/code.fish
@@ -1,4 +1,4 @@
 function code --description 'Launches codium with wayland flags'
-  __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only code-oss --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto $argv >/dev/null 2>&1 &
+  code-oss --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto $argv >/dev/null 2>&1 &
   disown
 end
