9b14ad246335b1c5bf7971d4c73bdd50aa250965
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-04-08 21:24:03 +0100

    Remove code function
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- files/.config/fish/functions/code.fish
+++
@@ -1,4 +0,0 @@
-function code --description 'Launches codium with wayland flags'
-  code-oss --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto $argv >/dev/null 2>&1 &
-  disown
-end
