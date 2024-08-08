2d1b4104c70ff2c26518b7beda2b29a1cf2e1109
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-08-08 21:40:00 +0100

    Fix indent
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,21 +1,21 @@
 if status is-interactive
-    # hydro
-    set -g hydro_color_pwd green
-    set -g hydro_color_prompt magenta
-    set -g hydro_color_duration yellow
+  # hydro
+  set -g hydro_color_pwd green
+  set -g hydro_color_prompt magenta
+  set -g hydro_color_duration yellow
 
-    # binary paths
-    fish_add_path ~/AppImages
-    fish_add_path ~/.local/bin
+  # binary paths
+  fish_add_path ~/AppImages
+  fish_add_path ~/.local/bin
 
-    # aliases
-    alias cat "bat --plain"
-    alias ls "eza --color=auto --header --git --icons"
-    alias prime-run "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only"
-    alias yay "without_user_paths yay"
-    alias pacman "without_user_paths pacman"
-    alias apt "without_user_paths apt"
+  # aliases
+  alias cat "bat --plain"
+  alias ls "eza --color=auto --header --git --icons"
+  alias prime-run "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only"
+  alias yay "without_user_paths yay"
+  alias pacman "without_user_paths pacman"
+  alias apt "without_user_paths apt"
 
-    # brew
-    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  # brew
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
