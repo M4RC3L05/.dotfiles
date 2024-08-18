f3dd36fb6c34396423b53d8c47f400775eed6163
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-08-18 01:36:13 +0100

    Use bat as manpager
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -4,6 +4,8 @@ if status is-interactive
   set -g hydro_color_prompt magenta
   set -g hydro_color_duration yellow
 
+  set -Ux MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+
   # aliases
   source $HOME/.alias
 
