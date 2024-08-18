c5af1fcb45d900ac356458027ff7b2d28c182f3e
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-08-18 15:07:05 +0100

    Add missing setup in docker test
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM debian:latest
 
-RUN apt update && apt install -y curl build-essential procps file git sudo passwd flatpak
+RUN apt update && apt install -y curl build-essential procps file git sudo passwd flatpak nano
 
 RUN groupadd -g 1000 main
 RUN useradd -m -u 1000 -g 1000 -s /bin/bash main && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,14 @@
 
 set -x
 
+export USER="$(whoami)"
+
 sudo dbus-daemon --system
 flatpak remote-add --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+git config --global user.email "foo@bar.com"
+git config --global user.name "foo"
+
+cd ~/.dotfiles && git add . && git commit -sm "wip" -n && cd
 
 exec "$@"
