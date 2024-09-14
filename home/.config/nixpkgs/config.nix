dd4626be6437d2d8728b811ea0aca741c02e1958
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-09-14 12:05:25 +0100

    Improve nix wrap binary
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- home/.config/nixpkgs/config.nix
+++ home/.config/nixpkgs/config.nix
@@ -2,35 +2,38 @@ let
   nixgl = import <nixgl> {};
   nixpkgs = import <nixpkgs-unstable> {};
 
-  # https://github.com/nix-community/nixGL/issues/44#issuecomment-1361524862
-  nixGLWrap = (nixGLCommand: pkg: nixpkgs.runCommand "${pkg.name}-${nixGLCommand.name}-wrapper" {} ''
+  # https://nixos.wiki/wiki/Nix_Cookbook#Wrapping_packages
+  wrap = { this, using }: nixpkgs.runCommand "${this.pkg.name}-${using.pkg.name}-wrapper" {
+    buildInputs = [nixpkgs.makeWrapper];
+  } ''
     mkdir $out
-    ln -s ${pkg}/* $out
-    rm $out/bin
-    mkdir $out/bin
-    for bin in ${pkg}/bin/*; do
-      wrapped_bin="$out/bin/$(basename $bin)"
-      printf "#!${nixpkgs.stdenv.shell}\n\nexec ${nixpkgs.lib.getExe nixGLCommand} %s \$@" "$bin" > $wrapped_bin
-      chmod +x $wrapped_bin
-    done
-  '');
 
-  waylandElectronWrapper = { pkg, disableGpu ? "false" }:
-    nixpkgs.runCommand "${pkg.name}-wayland-electron-wrapper" {} ''
-      mkdir $out
-      ln -s ${pkg}/* $out
-      rm $out/bin
-      mkdir $out/bin
-      for bin in ${pkg}/bin/*; do
-        wrapped_bin="$out/bin/$(basename $bin)"
-        gpuFlag=""
-        if [ "${disableGpu}" = "true" ]; then
-          gpuFlag="--disable-gpu"
-        fi
-        printf "#!${nixpkgs.stdenv.shell}\n\nexec %s --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform-hint=wayland %s \$@" "$bin" "$gpuFlag" > $wrapped_bin
-        chmod +x $wrapped_bin
-      done
-    '';
+    ln -s ${this.pkg}/* $out
+
+    rm $out/bin
+
+    mkdir $out/bin
+
+    ln -s ${this.pkg}/bin/* $out/bin
+
+    ${nixpkgs.lib.concatStringsSep " " (nixpkgs.lib.map(bin: ''
+      echo "=> Wrapping ${bin} using ${using.pkg.name}"
+
+      wrapped_source_path="${this.pkg}/bin/${bin}"
+      wrapped_destination_path="$out/bin/${bin}"
+      rm "$wrapped_destination_path"
+
+      ${if this.pkg.drvPath == using.pkg.drvPath then
+        ''
+          makeWrapper "$wrapped_source_path" "$wrapped_destination_path" --add-flags "$(printf "%s" "${nixpkgs.lib.concatStringsSep " " (this.flags or [])}")"
+        ''
+        else
+        ''
+          printf "#!${nixpkgs.stdenv.shell}\n\nexec ${nixpkgs.lib.getExe using.pkg} %s %s \"\$@\"" "$wrapped_source_path" "${nixpkgs.lib.concatStringsSep " " (this.flags or [])}" > $wrapped_destination_path
+          chmod +x $wrapped_destination_path
+        ''}
+    '') this.bins or [this.pkg.meta.mainProgram])}
+  '';
 in {
   allowUnfree = true;
 
@@ -46,7 +49,18 @@ in {
           nixgl.auto.nixVulkanNvidia
           nixgl.nixVulkanIntel
 
-          (waylandElectronWrapper { pkg = (nixGLWrap nixgl.nixGLIntel nixpkgs.vscode); })
+          (wrap {
+            this = {
+              pkg = nixpkgs.vscode;
+              flags = [
+                "--enable-features=UseOzonePlatform,WaylandWindowDecorations"
+                "--ozone-platform-hint=wayland"
+              ];
+            };
+            using = {
+              pkg = nixgl.nixGLIntel;
+            };
+          })
           nixpkgs.ffmpeg_7-full
           nixpkgs.bat
           nixpkgs.btop
@@ -63,21 +77,54 @@ in {
           nixpkgs.kubectl
           nixpkgs.lsof
           nixpkgs.vim
-          (nixGLWrap nixgl.auto.nixGLNvidia nixpkgs.nvtopPackages.full)
+          (wrap {
+            this = {
+              pkg = nixpkgs.nvtopPackages.full;
+            };
+            using = {
+              pkg = nixgl.auto.nixGLNvidia;
+            };
+          })
           nixpkgs.rsync
           nixpkgs.stow
           nixpkgs.tldr
           nixpkgs.tokei
           nixpkgs.tree
           nixpkgs.yt-dlp
-          (nixGLWrap nixgl.nixGLIntel nixpkgs.mpv)
+          (wrap {
+            this = {
+              pkg = nixpkgs.mpv;
+            };
+            using = {
+              pkg = nixgl.nixGLIntel;
+            };
+          })
           nixpkgs.cascadia-code
           nixpkgs.syncthing
-          (nixGLWrap nixgl.nixVulkanIntel nixpkgs.zed-editor)
+          (wrap {
+            this = {
+              pkg = nixpkgs.zed-editor;
+            };
+            using = {
+              pkg = nixgl.nixVulkanIntel;
+            };
+          })
           nixpkgs.oha
           nixpkgs.any-nix-shell
           nixpkgs.docker-credential-helpers
-          (waylandElectronWrapper { pkg = nixpkgs.youtube-music; disableGpu = "true"; })
+          (wrap {
+            this = {
+              pkg = nixpkgs.youtube-music;
+              flags = [
+                "--enable-features=UseOzonePlatform,WaylandWindowDecorations"
+                "--ozone-platform-hint=wayland"
+                "--disable-gpu"
+              ];
+            };
+            using = {
+              pkg = nixpkgs.youtube-music;
+            };
+          })
 
           (writeScriptBin "nix-rebuild" ''
             #!${stdenv.shell}
