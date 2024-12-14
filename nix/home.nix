9743673af9ef8a070b503da7a73b5bf309caf964
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-12-14 20:47:43 +0000

    Use `baseSessionVariables` to reuse on `sessionVariables`
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- nix/home.nix
+++ nix/home.nix
@@ -59,6 +59,31 @@ let
         ) bins
       )}
     '';
+
+  baseSessionVariables = {
+    # misc
+    EDITOR = "micro";
+
+    # docker
+    DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
+
+    #firefox
+    MOZ_ENABLE_WAYLAND = 1;
+
+    # bin paths
+    PATH = "$PATH:$HOME/AppImages:$HOME/.local/bin";
+
+    # locale stuff
+    # LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive"
+
+    # prefer wayland
+    QT_QPA_PLATFORM = "wayland;xcb";
+    CLUTTER_BACKEND = "wayland";
+    SDL_VIDEODRIVER = "wayland,x11";
+
+    # bitwarden
+    SSH_AUTH_SOCK = "$HOME/.bitwarden-ssh-agent.sock";
+  };
 in
 {
   home.username = "main";
@@ -149,30 +174,7 @@ in
     };
   };
 
-  home.sessionVariables = {
-    # misc
-    EDITOR = "micro";
-
-    # docker
-    DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
-
-    #firefox
-    MOZ_ENABLE_WAYLAND = 1;
-
-    # bin paths
-    PATH = "$PATH:$HOME/AppImages:$HOME/.local/bin";
-
-    # locale stuff
-    # LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive"
-
-    # prefer wayland
-    QT_QPA_PLATFORM = "wayland;xcb";
-    CLUTTER_BACKEND = "wayland";
-    SDL_VIDEODRIVER = "wayland,x11";
-
-    # bitwarden
-    SSH_AUTH_SOCK = "$HOME/.bitwarden-ssh-agent.sock";
-  };
+  home.sessionVariables = baseSessionVariables;
 
   fonts = {
     fontconfig = {
@@ -431,30 +433,7 @@ in
           };
         };
       };
-      sessionVariables = {
-        # misc
-        EDITOR = "micro";
-
-        # docker
-        DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
-
-        #firefox
-        MOZ_ENABLE_WAYLAND = 1;
-
-        # bin paths
-        PATH = "$PATH:$HOME/AppImages:$HOME/.local/bin";
-
-        # locale stuff
-        # LOCALE_ARCHIVE = "/usr/lib/locale/locale-archive"
-
-        # prefer wayland
-        QT_QPA_PLATFORM = ''"wayland;xcb"'';
-        CLUTTER_BACKEND = "wayland";
-        SDL_VIDEODRIVER = ''"wayland,x11"'';
-
-        # bitwarden
-        SSH_AUTH_SOCK = "$HOME/.bitwarden-ssh-agent.sock";
-      };
+      sessionVariables = baseSessionVariables;
       timers = {
         dump-packages = {
           Unit = {
