fcff57bee2d9944624fd59c5ca8f22979020084a
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2024-12-15 15:49:01 +0000

    Config readline
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- nix/home.nix
+++ nix/home.nix
@@ -166,9 +166,6 @@ in
       source = ~/.dotfiles/home/.local;
       recursive = true;
     };
-    ".inputrc" = {
-      source = ~/.dotfiles/home/.inputrc;
-    };
     ".eget.toml" = {
       source = ~/.dotfiles/home/.eget.toml;
     };
@@ -520,6 +517,31 @@ in
 
     nix-your-shell = {
       enable = true;
+    };
+
+    readline = {
+      enable = true;
+      includeSystemConfig = true;
+      bindings = {
+        "\\e[A" = "history-search-backward";
+        "\\e[B" = "history-search-forward";
+        "\\e[1;5D" = "backward-word";
+        "\\e[1;5C" = "forward-word";
+        TAB = "menu-complete";
+        "\\e[Z" = "menu-complete-backward";
+      };
+      variables = {
+        "bell-style" = "none";
+        "colored-stats" = "on";
+        "visible-stats" = "on";
+        "mark-symlinked-directories" = "on";
+        "colored-completion-prefix" = "on";
+        "menu-complete-display-prefix" = "on";
+        "show-all-if-ambiguous" = "on";
+        "completion-ignore-case" = "on";
+        "completion-map-case" = "on";
+        "skip-completed-text" = "on";
+      };
     };
 
     ssh = {
