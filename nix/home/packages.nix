b59371264d07e9226772b3761bd1d1e651f4583c
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2025-01-08 00:06:30 +0000

    Sort nix packages
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- nix/home/packages.nix
+++ nix/home/packages.nix
@@ -9,25 +9,33 @@
   internal.pkgs.theOfficeQuote
 
   pkgs.act
+  pkgs.bitwarden-cli
   pkgs.cascadia-code
   pkgs.curl
   pkgs.dive
   pkgs.docker-credential-helpers
   pkgs.ffmpeg_7-full
+  pkgs.hyperfine
   pkgs.kubectl
   pkgs.lsof
   pkgs.nil
   pkgs.nixfmt-rfc-style
+  pkgs.procps
   pkgs.q
   pkgs.rsync
   pkgs.tldr
   pkgs.tokei
   pkgs.wget
-  pkgs.procps
-  pkgs.hyperfine
   pkgs.wrk
   pkgs.zip
-  pkgs.bitwarden-cli
+
+  (config.lib.nixGL.wrappers.mesa (
+    utils.wrap pkgs.bitwarden-desktop {
+      env = {
+        NIXOS_OZONE_WL = "1";
+      };
+    }
+  ))
 
   (config.lib.nixGL.wrappers.nvidia pkgs.nvtopPackages.full)
 
@@ -37,14 +45,6 @@
         NIXOS_OZONE_WL = "1";
       };
       flags = [ "--disable-gpu" ];
-    }
-  ))
-
-  (config.lib.nixGL.wrappers.mesa (
-    utils.wrap pkgs.bitwarden-desktop {
-      env = {
-        NIXOS_OZONE_WL = "1";
-      };
     }
   ))
 ]
