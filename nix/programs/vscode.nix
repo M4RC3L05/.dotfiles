f4e19436885b94bf86172d317c53cce6e647392e
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2025-02-23 12:56:22 +0000

    Update vscode config to use profiles
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- nix/programs/vscode.nix
+++ nix/programs/vscode.nix
@@ -13,36 +13,40 @@
       environment = { inherit (pkgs.vscode) pname version meta; };
     }
   );
-  extensions = [
-    pkgs.vscode-extensions.denoland.vscode-deno
-    pkgs.vscode-extensions.tamasfe.even-better-toml
-    pkgs.vscode-extensions.skyapps.fish-vscode
-    pkgs.vscode-extensions.jnoortheen.nix-ide
-    pkgs.vscode-extensions.mads-hartmann.bash-ide-vscode
-    pkgs.vscode-extensions.editorconfig.editorconfig
-  ];
-  enableUpdateCheck = false;
-  enableExtensionUpdateCheck = true;
-  userSettings = {
-    "diffEditor.ignoreTrimWhitespace" = true;
-    "editor.cursorSmoothCaretAnimation" = "on";
-    "editor.fontFamily" = "'Cascadia Code', 'monospace', monospace";
-    "editor.fontLigatures" = true;
-    "editor.fontSize" = 13;
-    "editor.formatOnSave" = true;
-    "editor.smoothScrolling" = true;
-    "editor.tabSize" = 2;
-    "extensions.ignoreRecommendations" = true;
-    "files.autoSave" = "onFocusChange";
-    "telemetry.telemetryLevel" = "off";
-    "terminal.integrated.defaultProfile.linux" = "fish";
-    "terminal.integrated.fontFamily" = "'Cascadia Code NF', 'monospace', monospace";
-    "terminal.integrated.smoothScrolling" = true;
-    "window.autoDetectColorScheme" = true;
-    "window.dialogStyle" = "custom";
-    "window.titleBarStyle" = "custom";
-    "workbench.list.smoothScrolling" = true;
-    "workbench.preferredDarkColorTheme" = "Default Dark Modern";
-    "workbench.preferredLightColorTheme" = "Default Light Modern";
+  profiles = {
+    "default" = {
+      extensions = [
+        pkgs.vscode-extensions.denoland.vscode-deno
+        pkgs.vscode-extensions.tamasfe.even-better-toml
+        pkgs.vscode-extensions.skyapps.fish-vscode
+        pkgs.vscode-extensions.jnoortheen.nix-ide
+        pkgs.vscode-extensions.mads-hartmann.bash-ide-vscode
+        pkgs.vscode-extensions.editorconfig.editorconfig
+      ];
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = true;
+      userSettings = {
+        "diffEditor.ignoreTrimWhitespace" = true;
+        "editor.cursorSmoothCaretAnimation" = "on";
+        "editor.fontFamily" = "'Cascadia Code', 'monospace', monospace";
+        "editor.fontLigatures" = true;
+        "editor.fontSize" = 13;
+        "editor.formatOnSave" = true;
+        "editor.smoothScrolling" = true;
+        "editor.tabSize" = 2;
+        "extensions.ignoreRecommendations" = true;
+        "files.autoSave" = "onFocusChange";
+        "telemetry.telemetryLevel" = "off";
+        "terminal.integrated.defaultProfile.linux" = "fish";
+        "terminal.integrated.fontFamily" = "'Cascadia Code NF', 'monospace', monospace";
+        "terminal.integrated.smoothScrolling" = true;
+        "window.autoDetectColorScheme" = true;
+        "window.dialogStyle" = "custom";
+        "window.titleBarStyle" = "custom";
+        "workbench.list.smoothScrolling" = true;
+        "workbench.preferredDarkColorTheme" = "Default Dark Modern";
+        "workbench.preferredLightColorTheme" = "Default Light Modern";
+      };
+    };
   };
 }
