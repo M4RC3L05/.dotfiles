ece4d4318bd2dcf33dcb6f2f63ab0aafd13faa55
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2025-02-27 20:33:21 +0000

    Cleanup entrypoint and setup
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,9 +2,6 @@
 
 set -x
 
-flatpak remote-add --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add --user flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-
 git config --global user.email "foo@bar.com"
 git config --global user.name "foo"
 
