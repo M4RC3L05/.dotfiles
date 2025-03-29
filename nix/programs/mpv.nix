6eba976dd620bfe513001658b1c6e80b937c49c7
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2025-03-29 19:54:18 +0000

    Allow to use nvidia with mpv
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- nix/programs/mpv.nix
+++ nix/programs/mpv.nix
@@ -1,13 +1,14 @@
 { config, pkgs }:
 {
   enable = true;
-  package = config.lib.nixGL.wrappers.mesa pkgs.mpv;
+  package = config.lib.nixGL.wrappers.nvidia (config.lib.nixGL.wrappers.mesa pkgs.mpv);
   config = {
     force-window = true;
     profile = "gpu-hq";
     gpu-api = "auto";
     vo = "gpu-next";
     hwdec = "auto";
+    hwdec-codecs = "all";
     ytdl-format = "bestvideo+bestaudio";
   };
 }
