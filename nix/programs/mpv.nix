b8729ea55919cadb971ccc199c3765b09446c46b
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2025-04-04 22:51:24 +0100

    Enable youtube and vapoursynth to mpv
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- nix/programs/mpv.nix
+++ nix/programs/mpv.nix
@@ -1,7 +1,14 @@
 { config, pkgs }:
 {
   enable = true;
-  package = config.lib.nixGL.wrappers.nvidia (config.lib.nixGL.wrappers.mesa pkgs.mpv);
+  package = config.lib.nixGL.wrappers.nvidia (
+    config.lib.nixGL.wrappers.mesa (
+      pkgs.mpv-unwrapped.wrapper {
+        mpv = pkgs.mpv-unwrapped.override { vapoursynthSupport = true; };
+        youtubeSupport = true;
+      }
+    )
+  );
   config = {
     keepaspect = "no";
     force-window = "yes";
