055bb483aeadb44cebbe15d4aaff1d977569cfe4
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2026-02-19 19:11:05 +0000

    Use homebrew fish shell
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -22,14 +22,5 @@ status is-interactive; and begin
   brew shellenv | grep -Ev 'fish_add_path --global --move --path "' | source
   fish_add_path --append --global --move --path "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin";
 
-  if test -d (brew --prefix)/share/fish/completions
-    set -p fish_complete_path (brew --prefix)/share/fish/completions
-  end
-  
-  if test -d (brew --prefix)/share/fish/vendor_completions.d
-    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-  end
-
-  mise activate fish | source
   batman --export-env | source
 end
